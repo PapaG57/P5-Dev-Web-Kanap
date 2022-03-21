1c857ddc6711dda34ebd60b88f662f4a52c5bf11
Florent GERARD <florentgerard@sfr.fr>
2022-03-21 17:23:42 +0100

    git commit -m "affichage produits index.html"

--- front/js/script.js
+++ front/js/script.js
@@ -1,6 +1,20 @@
 function allProducts(){
     fetch("http://localhost:3000/api/products")
     .then(response=>response.json())
-    .then(data=>console.log(data))
+    .then(data=>affichage(data))
 }
 allProducts();
+
+function affichage(produit){
+for(p of produit){
+
+    const codeHtml=`<a href="./product.html?id=${p._id}">
+            <article>
+              <img src="${p.imageUrl}" alt="${p.altTxt}">
+              <h3 class="productName">${p.name}</h3>
+              <p class="productDescription">${p.description}.</p>
+            </article>
+          </a>`;
+          document.getElementById("items").insertAdjacentHTML("beforeend",codeHtml);  
+   }
+}
